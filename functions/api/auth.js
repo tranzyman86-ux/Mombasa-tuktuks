d0ab5b858bc04144a829d7445e301b0be13f7701
tranzyman86-ux <tranzyman86@gmail.com>
2026-08-16 23:12:24 +0300

    Delete functions/api directory

--- functions/api/auth.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  return await fetch(`https://decap-cms-oauth-proxy.herokuapp.com/callback`, {
-    method: context.request.method,
-    headers: context.request.headers,
-    body: context.request.body
-  });
-}
